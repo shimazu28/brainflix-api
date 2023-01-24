92d79b9b0a6db97a02e039cff28e366e4a78e378
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-01-23 21:50:47 -0500

    Ordering  was changed in server.js

--- server.js
+++ server.js
@@ -3,22 +3,19 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const videosRoute = require('./routes/videos')
-const bodyParser = require("body-parser");
 
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 5050;
 
 app.use(cors({ origin: process.env.ALLOWED_ORIGIN}));
-
-app.use('/videos', videosRoute, express.static('public'));
-
-
-
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/videos', videosRoute);
+
+
+
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
